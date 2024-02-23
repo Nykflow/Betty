6415e4fc17360222ad50a2ad2351bc44c4a69889
nykflow <nayntemesha@gmail.com>
2024-02-23 14:31:57 +0000

    abc

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int d;
+char c;
+
+for (d = '1'; d <= '9'; d++)
+putchar(d);
+for (c = 'a'; c <= 'f'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
