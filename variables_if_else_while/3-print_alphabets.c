98e2552c17e7b30386460e1d1c1f2c634488f233
nykflow <nayntemesha@gmail.com>
2024-02-23 08:52:47 +0000

    abc

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,15 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int main(void);
 {
-        char c;
-        
-        for (c = 'a'; c  <= 'z'; c++)
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-        for (c = 'A'; c <= 'Z'; c++)
+	for (c = 'A'; c <= 'Z'; c++)
 		putchar(c);
-        putchar('\n');
-        
-        return(0);   
+	putchar('\n');
+
+	return (0);
 }
