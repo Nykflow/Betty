462f35aec5460ea4cd88d32f70a367d99597fae3
nykflow <nayntemesha@gmail.com>
2024-02-26 15:14:24 +0000

    abc

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -3,15 +3,15 @@
 /**
  * _isalpha - Check Holberton
  * @c: An input character
- * Description: function returns 1 if the character is r
+ * Description: function returns 1 if the character is c
  * letter, lowercase or uppercase.
  * Return: 1 or 0 in otherwise.
  */
 int _isalpha(int c)
 {
-if (r >= 'A' && r <= 'Z')
+if (c >= 'A' && c <= 'Z')
 return (1);
-else if (r >= 'a' && r <= 'z')
+else if (c >= 'a' && c <= 'z')
 return (1);
 else
 return (0);
